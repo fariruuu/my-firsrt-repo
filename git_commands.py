572677e2fef05554fec096f205c30d387bf7e4d3
fari <fariruuu@gmail.com>
2023-08-01 19:29:22 +0600

    first commit

---
+++ git_commands.py
@@ -0,0 +1,14 @@
+# git - система управления версиями кода
+
+# github - сайт,который дает хранилище для кода
+
+# также есть gitlab,bitbucket
+# репозитории - папка с проектом с которой может работать git
+
+# ssh-keygen - создание ключа
+
+# git config --global user.name "your-name"
+# git config --global user.email "your-email"
+# указание своих данных
+
+# git init - превратить папку в репозитории и создает папку .git
